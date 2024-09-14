3da3de818d856154235199fe74218541dcd7ffef
Greight <vonabirg.e.a@gmail.com>
2024-09-14 23:42:35 +0300

    M6: Добавил swagger на адрес api/schema/swagger-ui

--- app/realty/views.py
+++ app/realty/views.py
@@ -1,10 +1,10 @@
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.views import APIView
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from realty.models import Flat
 from django.http import Http404
-
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 # Flat APIs
 
@@ -23,11 +23,15 @@ class FlatListAPIView(APIView):
         created_at = serializers.DateTimeField()
         last_update = serializers.DateTimeField()
 
+    @extend_schema(
+            summary="Получение полного списка всех квартир.",
+            description="""Очень удобное api для получение полного списка 
+            квартир которые когда-либол были добавлены на сайт.""",
+    )
     def get(self, request):
         flats = Flat.objects.all()
         serializer = FlatListAPIView.OutputSerializer(flats, many=True)
         return Response(serializer.data)
-        
 
 
 class FlatDetailAPIView(RetrieveAPIView):
@@ -47,11 +51,15 @@ class FlatDetailAPIView(RetrieveAPIView):
         except Flat.DoesNotExist:
             raise Http404
 
+    @extend_schema(
+            summary="Получение конкретной квартиры по её идентификатору flat_id.",
+            description="""Очень удобное api для получения конекртной квартиры 
+            по её уникальному иднетификатору flat_id.""",
+    )
     def get(self, request, flat_id):
         flat = self.get_object(flat_id)
         serializer = FlatDetailAPIView.OutputSerializer(flat)
         return Response(serializer.data)
-
 
 
 class FlatUpdateAPIView(UpdateAPIView):
